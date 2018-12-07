a4ca85c42d233802df4dd3b088207db19636c249
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-12-07 10:15:21 +0530

    add build

---
+++ src/main/java/com/demo/dronedemo/DronedemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.dronedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronedemoApplication.class, args);
+	}
+}
